8a46b6c112d1ca34e99d71c21ed103666123204a
David Mastropolo <dave.mastropolo@gmail.com>
2023-07-24 14:18:24 -0700

    added many other resources to the main.tf like security group, sg rule, subnet, security group association to the subnet

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-#Azure provider to start out with
+#Azure required provider(s) to start out with
 # this allows terraform to communicate with the Azure API
 # this is how terraform can deploy resources to Azure.  This is
 # very similar to the AWS provider setup for terraform.
@@ -12,6 +12,7 @@ terraform {
       version = "=2.97.0"
     }
   }
+
 }
 
 # Configure the Microsoft Azure Provider
@@ -48,4 +49,55 @@ resource "azurerm_virtual_network" "mtc-vn" {
   tags = {
     environment = "dev"
   }
+}
+
+# Subnet
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/subnet
+resource "azurerm_subnet" "mtc-subnet" {
+  name                 = "mtc-subnet"
+  resource_group_name  = azurerm_resource_group.mtc-rg.name
+  virtual_network_name = azurerm_virtual_network.mtc-vn.name
+  address_prefixes     = ["10.123.1.0/24"]
+  # note that like the CIDR block on the virtual network, this is also a list
+  # so one can have many subnets defined here within that CIDR block of the virtual network.
+}
+
+# Security group (we will be adding as separate resource, rather than inline)
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_security_group
+resource "azurerm_network_security_group" "mtc-sg" {
+  name                = "mtc-sg"
+  location            = azurerm_resource_group.mtc-rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+# Network security rule
+# this rule will allow for development access to the Azure dev-node
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/network_security_rule
+resource "azurerm_network_security_rule" "mtc-dev-rule" {
+  name                        = "mtc-dev-rule"
+  priority                    = 100 #direction                   = "Outbound"
+  direction                   = "Inbound"
+  access                      = "Allow" #protocol                    = "Tcp"
+  protocol                    = "*"     # this will allow ICMP, etc as well.
+  source_port_range           = "*"
+  destination_port_range      = "*"                #source_address_prefix       = "*"
+  source_address_prefix       = "98.234.32.176/32" # we want to limit this to my own ip address.
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.mtc-rg.name
+  network_security_group_name = azurerm_network_security_group.mtc-sg.name
+}
+
+# Security group association
+# Need to associate the subnet with the security group and rules above
+# so that the subnet can be protected by the rules
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/subnet_network_security_group_association
+resource "azurerm_subnet_network_security_group_association" "mtc-sga" {
+  subnet_id                 = azurerm_subnet.mtc-subnet.id
+  network_security_group_id = azurerm_network_security_group.mtc-sg.id
+  # note that we are referencing the ids of both, not the name. The ids are assigned by Azure
+  # and are know after the terraform apply and instantiation of the resource.
 }
