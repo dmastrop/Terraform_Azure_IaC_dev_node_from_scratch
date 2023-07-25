85c1e34e6351a3125fb3786faf588ad3f844611b
David Mastropolo <dave.mastropolo@gmail.com>
2023-07-25 12:49:16 -0700

    variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 # in my case host_os can be either:
 # windows OR
 # linux-mac
+# the file names must be of the form command = templatefile("${var.host_os}-ssh-script.tpl", {
 variable "host_os" {
     type = string
 }
