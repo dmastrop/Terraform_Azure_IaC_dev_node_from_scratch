836fc27da1d556f661bf0807fdf92cf49037c262
David Mastropolo <dave.mastropolo@gmail.com>
2023-07-24 21:10:31 -0700

    windows-ssh-script.tpl and operating system variable added

--- main.tf
+++ main.tf
@@ -197,8 +197,14 @@ provisioner "local-exec" {
   }
 }
 
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/public_ips
+# https://developer.hashicorp.com/terraform/language/data-sources
 data "azurerm_public_ip" "mtc-ip-data" {
     name = azurerm_public_ip.mtc-ip.name
     resource_group_name = azurerm_resource_group.mtc-rg.name
+}
+
+output "public_ip_address" {
+  value = "${azurerm_linux_virtual_machine.mtc-vm.name}: ${data.azurerm_public_ip.mtc-ip-data.ip_address}"
 }
 
